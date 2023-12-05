dc1b88fc676faa20bc5caef8e5fcd7236cfbfbfb
Arisha Seraj <sss6462278@gmail.com>
2023-12-05 19:43:00 +0500

    dtfufgihoiu

--- session-demo/fir-add-queries.php
+++ session-demo/fir-add-queries.php
@@ -30,12 +30,12 @@ if (isset($_POST['add-customer'])) {
 
     if ($file_extension == 'pdf') {
         move_uploaded_file($file_tmp, $photo_dir);
-        $add_customer_query = mysqli_query($cn, "INSERT INTO `property_selling`( `name`, `father_name`, `cnic`, `mobile`, `address`, `plot_no`,`installment`,`price`, `advance`, `remaning`,`image` ,`status`  ) 
+        $add_customer_query = mysqli_query($cn, "INSERT INTO `property_selling`(`name`, `father_name`,`cnic`,`mobile`,`address`,`plot_no`,`installment`,`price`, `advance`,`remaning`,`image`,`status`) 
          VALUES ('$name','$father_name','$cnic','$mobile','$address','$plot_no', 
          '$installment','$price','$advance','$remaning','$photo_dir','1')");
-            echo "<script>alert('pdf file type is uploaded.....')
-        window.location.href='add-customer.php'
-        </script> ";
+          echo "<script>alert('pdf file type is uploaded.....')
+          window.location.href='add-customer.php'
+          </script> ";
     } else {
         echo "<script>alert('only pdf files are uploaded.....')
         window.location.href='add-customer.php'
