7876da6f7a60977860c0051e914c482fdbad857e
Arisha Seraj <sss6462278@gmail.com>
2023-12-08 13:04:40 +0500

    active inactive done

--- session-demo/inactive-status.php
+++ session-demo/inactive-status.php
@@ -1,21 +1,25 @@
 <?php include('config.php');
 $get_id = $_GET['id'];
 
-$qry = "SELECT * FROM property_selling";
+$qry = "SELECT * FROM property_selling WHERE id='$get_id'";
 $result = mysqli_query($cn, $qry);
 if(mysqli_num_rows($result)> 0){
     $row = mysqli_fetch_assoc($result);
+    $id = $row['id'];
     $status = $row['status'];
-    if($status == '0'){
-        $customer_delete_query = mysqli_query($cn, "UPDATE property_selling SET status = '1' WHERE id='$get_id'");
 
+    if($status == '1'){
+        $customer_delete_query = mysqli_query($cn, "UPDATE property_selling SET status = '0' WHERE id='$get_id'");
         if($customer_delete_query){
             session_start();
             $_SESSION['status'] = true;
             header("Location: view-customer.php");
+            mysqli_error($cn);
         }
         else{
-            header("Location:view-customer.php");
+header("Location:view-customer.php");
+            mysqli_error($cn);
+
         }
     }
 }
